e4852ba4e56401b8109432145207b3876e881a0c
Shmuel <services@raichman.io>
2024-11-29 11:17:52 +0200

    Added - entrypoint to change server url at client startup

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -3,7 +3,7 @@
 
 # Replace wehn contianer startup the server URL in all files.
 for file in $(grep --color=never -r "server-app:3000" /usr/share/nginx/html/**/*  | awk -F ':' '{print $1}'); do 
-    sed -i "s|https://server-app:3000/|${SERVER_URL}/|g" $file | grep 1449
+    sed -i "s|https://server-app:3000/|${SERVER_URL}/|g" $file
 done
 
 # Run default entrypoint
